18fbe55a75d33fffc960f353d7bbc55abb6f8d79
Thomas Marescal <thomas.marescal@gmail.com>
2020-05-04 11:16:23 +0400

    Updates

---
+++ precache-manifest.10df4983696cc17af5bf79f0421d5652.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "884ef800c4931be21d5b6af4ed70cdb6",
+    "url": "/recette-manager/index.html"
+  },
+  {
+    "revision": "d1cc4792d74e6c47e849",
+    "url": "/recette-manager/static/css/main.7e870e2b.chunk.css"
+  },
+  {
+    "revision": "b9ca5c81812eb43d703e",
+    "url": "/recette-manager/static/js/2.35ddcb49.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/recette-manager/static/js/2.35ddcb49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1cc4792d74e6c47e849",
+    "url": "/recette-manager/static/js/main.f7fc8193.chunk.js"
+  },
+  {
+    "revision": "60b98214e091df6857e8",
+    "url": "/recette-manager/static/js/runtime-main.ec0979c6.js"
+  },
+  {
+    "revision": "ef6f8e5937cde4dd88d6b51d1b023ed9",
+    "url": "/recette-manager/static/media/waltographUI.ef6f8e59.ttf"
+  }
+]);
